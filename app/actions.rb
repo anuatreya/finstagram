80215c6649b481b97c1102189e777f87fb3bb778
anuatreya <anupamait02@gmail.com>
2022-02-22 06:59:05 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
@@ -10,22 +16,42 @@ get '/' do
 
   post '/signup' do
 
-    # grab user input values from params
     email      = params[:email]
     avatar_url = params[:avatar_url]
     username   = params[:username]
     password   = params[:password]
-
-     # instantiate and save a User
+  
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
-    
-
-     # if user validations pass and user is saved
-     if @user.save
-      "User #{username} saved!"
+  
+    if @user.save
+      redirect to('/login')
     else
       erb(:signup)
-   
+    end
+
   end
-    
+
+  get '/login' do    # when a GET request comes into /login
+    erb(:login)      # render app/views/login.erb
+  end
+
+  post '/login' do    # when we submit a form with an action of /login
+     username = params[:username]
+     password = params[:password]
+
+     @user = User.find_by(username: username)
+
+     if @user && @user.password == password
+       session[:user_id] = @user.id
+       redirect to('/')
+       
+     else
+      @error_message = "Login failed."
+      erb(:login)
+    end
+  end
+
+  get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
   end
